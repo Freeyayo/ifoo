3c25e7dece285fcaf1a00dfb38847124e1b4cd55
kevincai1 <kecincai@aaxischina.com>
2020-06-22 21:13:45 +0800

    add compose

---
+++ src/utils/functions/normal.ts
@@ -0,0 +1,26 @@
+/*
+ * @Author: Conghao Cai🔧
+ * @Date: 2020-06-22 19:44:17
+ * @LastEditTime: 2020-06-22 21:08:03
+ * @LastEditors: Please set LastEditors
+ * @FilePath: \ifoo\src\utils\functions\normal.ts
+ */ 
+import { CONSOLE_HEADER_TEXT, CONSOLE_HEADER_STYLE } from '../../global_data';
+import { PureFunctionCompose } from '../functions/types/function_types';
+/**
+ * @description: a pipeline for functions runs a 'left to right' order
+ * @param {funtion} 
+ * @return: a composed function
+ */
+
+export const compose: PureFunctionCompose<Function> = (...fns) => x => fns.reduce((acc, f)=> {
+    if(typeof f !== "function" || f.length !== 1){
+        console.log(CONSOLE_HEADER_TEXT, CONSOLE_HEADER_STYLE, `compose accepts pure function(s) which needs only 1 argument`);
+    }
+    try{
+        return f(acc);
+    }catch(e){
+        console.log(CONSOLE_HEADER_TEXT, CONSOLE_HEADER_STYLE, e);
+    }
+}, x);
+
