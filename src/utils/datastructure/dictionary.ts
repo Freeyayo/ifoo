a1b0a6646b2e7b590072e311d1b62fcb26a45581
Sergiu M <sergiu.munteanu@live.com>
2020-07-02 00:39:17 -0400

    Fix type for entires method

--- src/utils/datastructure/dictionary.ts
+++ src/utils/datastructure/dictionary.ts
@@ -11,84 +11,84 @@ import { KeyValue, IDictionary } from "./types/data_interfaces";
  *
  */
 class Dictionary implements IDictionary {
-  // The object that actualy stored the key-values
-  _items: KeyValue = {};
+    // The object that actualy stored the key-values
+    _items: KeyValue = {};
 
-  // So you can do mydictionary.length
-  length: number = 0;
+    // So you can do mydictionary.length
+    length: number = 0;
 
-  constructor() {
-    // TODO: Accept an object or array for initialization
-  }
-
-  _getKeyAsString(key: any): string {
-    if (key === undefined) {
-      throw new TypeError("Spurv: key is undefined");
+    constructor() {
+        // TODO: Accept an object or array for initialization
     }
-    if (typeof key === "object") {
-      return JSON.stringify(key);
+
+    _getKeyAsString(key: any): string {
+        if (key === undefined) {
+            throw new TypeError("Spurv: key is undefined");
+        }
+        if (typeof key === "object") {
+            return JSON.stringify(key);
+        }
+        return key.toString();
     }
-    return key.toString();
-  }
 
-  // Add a key value to the dictionary
-  add(key: any, value: any) {
-    const keyString: string = this._getKeyAsString(key);
-    if (!(keyString in this._items)) {
-      this.length++;
+    // Add a key value to the dictionary
+    add(key: any, value: any) {
+        const keyString: string = this._getKeyAsString(key);
+        if (!(keyString in this._items)) {
+            this.length++;
+        }
+        this._items[keyString] = value;
     }
-    this._items[keyString] = value;
-  }
 
-  // Clear the dictionary
-  clear() {
-    this._items = {};
-    this.length = 0;
-  }
-
-  // Check if the dictionary contains a key
-  containsKey(key: any): boolean {
-    const keyString: string = this._getKeyAsString(key);
-    return Object.keys(this._items).includes(keyString);
-  }
-
-  // Check if the dictionary contains a value (works only for primitives)
-  containsValue(key: any): boolean {
-    const keyString: string = this._getKeyAsString(key);
-    return Object.values(this._items).includes(keyString);
-  }
-
-  // Get value by key
-  get(key: any): any {
-    const keyString: string = this._getKeyAsString(key);
-    return this._items[keyString] || undefined;
-  }
-
-  // Remove a key
-  remove(key: any): void {
-    const keyString: string = this._getKeyAsString(key);
-    if (keyString in this._items) {
-      this.length--;
+    // Clear the dictionary
+    clear() {
+        this._items = {};
+        this.length = 0;
     }
-    delete this._items[keyString];
-  }
 
-  // Returns an array of keys
-  keys(): string[] {
-    return Object.keys(this._items);
-  }
+    // Check if the dictionary contains a key
+    containsKey(key: any): boolean {
+        const keyString: string = this._getKeyAsString(key);
+        return Object.keys(this._items).includes(keyString);
+    }
 
-  // Returns an array of entries [key, value]
-  entries(): KeyValue {
-    return Object.entries(this._items);
-  }
+    // Check if the dictionary contains a value (works only for primitives)
+    containsValue(key: any): boolean {
+        const keyString: string = this._getKeyAsString(key);
+        return Object.values(this._items).includes(keyString);
+    }
 
-  // Returns an array of objects
-  values(): any[] {
-    return Object.values(this._items);
-  }
+    // Get value by key
+    get(key: any): any {
+        const keyString: string = this._getKeyAsString(key);
+        return this._items[keyString] || undefined;
+    }
+
+    // Remove a key
+    remove(key: any): void {
+        const keyString: string = this._getKeyAsString(key);
+        if (keyString in this._items) {
+            this.length--;
+        }
+        delete this._items[keyString];
+    }
+
+    // Returns an array of keys
+    keys(): string[] {
+        return Object.keys(this._items);
+    }
+
+    // Returns an array of entries [key, value]
+    entries(): KeyValue[] {
+        return Object.entries(this._items);
+    }
+
+    // Returns an array of objects
+    values(): any[] {
+        return Object.values(this._items);
+    }
 }
 
 export const dictionary = () => {
-  return new Dictionary();
+    return new Dictionary();
 };
