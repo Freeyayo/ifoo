af61aca03650811f0fe6a7d1515c2da7570e4634
caiconghao <caiconghao@icloud.com>
2020-07-11 21:46:13 +0800

    test/curry

--- test/curry.test.js
+++ test/curry.test.js
@@ -5,6 +5,21 @@ const foo = (a,b,c,d) => [a,b,c,d];
 test("curry a function", () => {
   const outputString = JSON.stringify([1,2,3,4]);
   const curriedFoo = curry(foo);
+  
   const temp1 = curriedFoo(1,2);
-  expect(temp1(3,4)).toBe(outputString)
+  expect(JSON.stringify(temp1(3,4))).toBe(outputString)
+  
+  const temp2 = curriedFoo(1)(2);
+  expect(JSON.stringify(temp2(3,4))).toBe(outputString)
+});
+
+test("curry a function with some arguments", () => {
+  const outputString = JSON.stringify([1,2,3,4]);
+  const curriedFoo = curry(foo,1,2);
+  
+  const temp1 = curriedFoo(3,4);
+  expect(JSON.stringify(temp1)).toBe(outputString)
+  
+  const temp2 = curriedFoo(3)(4);
+  expect(JSON.stringify(temp2)).toBe(outputString)
 });
